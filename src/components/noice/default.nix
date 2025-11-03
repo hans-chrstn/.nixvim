e24b5236f341d21f09c7dc1419d680103bfd19ad
hayato-oo <xuhiko13@gmail.com>
2025-11-03 12:44:46 -0500

    (enhancements): added round borders

--- src/components/noice/default.nix
+++ src/components/noice/default.nix
@@ -19,61 +19,121 @@ in {
         priority = 50;
         setupModule = "noice";
         setupOpts = {
-          notify = {
-            enabled = false;
+          cmdline = {
+            enabled = true;
+            view = "cmdline_popup";
+            format = {
+              cmdline = {
+                pattern = "^:";
+                icon = "";
+                lang = "vim";
+              };
+              search_down = {
+                kind = "search";
+                pattern = "^/";
+                icon = " ";
+                lang = "regex";
+              };
+              search_up = {
+                kind = "search";
+                pattern = "^%?";
+                icon = " ";
+                lang = "regex";
+              };
+              filter = {
+                pattern = "^:%s*!";
+                icon = "$";
+                lang = "bash";
+              };
+              lua = {
+                pattern = "^:%s*lua%s+";
+                icon = "";
+                lang = "lua";
+              };
+              help = {
+                pattern = "^:%s*he?l?p?%s+";
+                icon = "";
+              };
+            };
           };
+
           messages = {
             enabled = true;
           };
+
+          popupmenu = {
+            enabled = true;
+            backend = "nui";
+          };
+
+          notify = {
+            enabled = false;
+          };
+
           lsp = {
+            progress = {
+              enabled = false;
+            };
             override = {
               "vim.lsp.util.convert_input_to_markdown_lines" = true;
               "vim.lsp.util.stylize_markdown" = true;
               "cmp.entry.get_documentation" = true;
             };
+            hover = {
+              enabled = true;
+            };
+            signature = {
+              enabled = true;
+            };
             message = {
               enabled = true;
             };
-            progress = {
-              enabled = false;
-              view = "mini";
+          };
+
+          views = {
+            cmdline_popup = {
+              border = {
+                style = "rounded";
+                padding = [0 1];
+              };
+              position = {
+                row = "50%";
+                col = "50%";
+              };
+            };
+            popupmenu = {
+              border = {
+                style = "rounded";
+                padding = [0 1];
+              };
+            };
+            hover = {
+              border = {
+                style = "rounded";
+              };
+            };
+            signature = {
+              border = {
+                style = "rounded";
+              };
+            };
+            confirm = {
+              border = {
+                style = "rounded";
+              };
             };
           };
-          popupmenu = {
-            enabled = true;
-            backend = "nui";
-          };
-          presets = {
-            bottom_search = true;
-            command_palette = true;
-            long_message_to_split = true;
-          };
-          format = {
-            filter = {
-              pattern = [
-                ":%s*%%s*s:%s*"
-                ":%s*%%s*s!%s*"
-                ":%s*%%s*s/%s*"
-                "%s*s:%s*"
-                ":%s*s!%s*"
-                ":%s*s/%s*"
-              ];
-              icon = "";
-              lang = "regex";
-            };
-            replace = {
-              pattern = [
-                ":%s*%%s*s:%w*:%s*"
-                ":%s*%%s*s!%w*!%s*"
-                ":%s*%%s*s/%w*/%s*"
-                "%s*s:%w*:%s*"
-                ":%s*s!%w*!%s*"
-                ":%s*s/%w*/%s*"
-              ];
-              icon = "󱞪";
-              lang = "regex";
-            };
-          };
+
+          routes = [
+            {
+              filter = {
+                event = "msg_show";
+                kind = "";
+                find = "written";
+              };
+              opts = {skip = true;};
+            }
+          ];
         };
       };
     };
